b3ecdc45a39e76cca04a2c448577a54fbdd66891
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-04-09 07:17:19 +0000

    sum

---
+++ sum.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter a no."))
+num2 = int(input("enter a second no."))
+
+print("sum =" ,num1+num2)
+print("diffrence =" ,num1-num2)
+print("division =" ,num1%num2)
+print("multiplication =" ,num1*num2)
