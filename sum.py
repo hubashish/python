b894500c827ae4107a9a0fe41f5811374b36a844
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-04-09 07:31:57 +0000

    sum

--- sum.py
+++ sum.py
@@ -5,3 +5,13 @@ print("sum =" ,num1+num2)
 print("diffrence =" ,num1-num2)
 print("division =" ,num1%num2)
 print("multiplication =" ,num1*num2)
+
+length = float(input("side of first area :"))
+breadth = float(input("side of second area :"))
+
+print("area of rectangle =" , length*breadth) 
+
+
+side = float(input("area of side :"))
+
+print("area =" , side*side)
